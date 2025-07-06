28f1145a3d38cb59804408cc4aa8351ee6531ab9
jaysheng <1534740320@qq.com>
2025-07-06 21:23:58 +0800

    feat(PluginsUi): 删除项目配置及文件

--- main.cpp
+++ main.cpp
@@ -2,29 +2,9 @@
 #include <QSharedPointer>
 #include <QTableView>
 
-#include "src/core/engine/table_engine.h"
-#include "src/plugins/excel/excel_importer.h"
-#include "src/ui/main_window.h"
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    auto w = new MainWindow;
-    w->show();
-
-    auto *engine = new TableEngine(w);
-    engine->registerImporter(QSharedPointer<Plugins::FileImporterInterface>(new Plugins::ExcelImporter()));
-
-    (void) QObject::connect(engine, &TableEngine::dataLoaded, [engine, w](const bool success) {
-        if (success) {
-            auto *tableView = new QTableView(w);
-            tableView->setModel(engine->dataModel());
-            w->setCentralWidget(tableView);
-        }
-    });
-
-    qDebug() << engine->loadFromFile("D:/workspace/PluginsUi/test_data/datasets.xlsx");
-
 
 
     return QApplication::exec();
