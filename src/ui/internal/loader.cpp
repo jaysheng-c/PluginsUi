bc2bc8f42c16722d3d911349ced192b7d85baf61
jaysheng <1534740320@qq.com>
2025-07-16 23:50:16 +0800

    feat(ui): - 增加资源文件 - MainWindow增加右侧工具栏 - 简单实现工具栏逻辑

--- src/ui/internal/loader.cpp
+++ src/ui/internal/loader.cpp
@@ -11,6 +11,10 @@
 
 #include "loader.h"
 
+#include <QDockWidget>
+#include <QToolBar>
+#include <QToolButton>
+
 #include "main_window.h"
 #include "table/table_container.h"
 #include "table/table_data.h"
@@ -26,6 +30,64 @@ UiLoader::UiLoader(QObject *parent)
 {
     TableData::initFontMap();
     m_mainWindow->setWidgetContainer(new TableContainer(this));
+
+    if (auto *toolBar = m_mainWindow->findChild<QToolBar*>("right_tool_bar")) {
+        toolBar->setStyleSheet(
+        "QToolBar {"
+                "background-color: #252526;         /* 深灰背景 */\n"
+                "color: #d4d4d4;                    /* 浅灰文字 */\n"
+                "spacing: 8px;\n"
+                "padding: 2px 2px;\n"
+                "border-bottom: 1px solid #333333;  /* 底部边框线 */\n"
+            "}\n\n"
+            "QToolBar QToolButton {\n"
+            "   width: 32px;\n"
+            "   height: 32px;\n"
+            "   background: transparent;\n"
+            "   border-radius: 5px;\n"
+            "}\n\n"
+            "QToolBar QToolButton:hover {\n"
+            "   background-color: #3e3e42;      /* 悬停背景色 */\n"
+            "}\n\n"
+            "QToolBar QToolButton:checked {\n"
+            "    background-color: #1083ee;     /* 蓝色选中效果 */\n"
+            "}\n\n"
+            "QToolBar QToolButton:checked:hover {\n"
+            "    background-color: #1083ee;     /* 保持蓝色选中状态 */\n"
+            "}\n\n"
+            "QToolBar::separator {\n"
+            "    width: 5px;\n"
+            "    background: transparent;\n"
+            "}"
+            );
+
+        // toolBar->setIconSize(QSize(32, 32));
+        toolBar->setFixedWidth(36);
+        toolBar->setMovable(false);
+        toolBar->setFloatable(false);
+        m_mainWindow->addToolBar(Qt::RightToolBarArea, toolBar);
+
+        auto *action = toolBar->addAction(QIcon(":/image/ai.png"), "Ai", [=](const bool checked) {
+            auto *docker = m_mainWindow->findChild<QDockWidget*>("AiDocker");
+            if (!docker) {
+                docker = new QDockWidget("AiDocker", m_mainWindow);
+                docker->setObjectName("AiDocker");
+                docker->setAllowedAreas(Qt::RightDockWidgetArea);
+                m_mainWindow->addDockWidget(Qt::RightDockWidgetArea, docker);
+                (void) connect(docker, &QDockWidget::visibilityChanged, [=](const bool visible) {
+                    if (auto *aiAction = toolBar->findChild<QAction*>("Ai")) {
+                        aiAction->setChecked(visible);
+                    }
+                });
+            }
+            docker->setVisible(checked);
+            if (auto *aiAction = toolBar->findChild<QAction*>("Ai")) {
+                aiAction->setIcon(checked ? QIcon(":/image/ai_selected.png") : QIcon(":/image/ai.png"));
+            }
+        });
+        action->setObjectName("Ai");
+        action->setCheckable(true);
+    }
 }
 
 UiLoader::~UiLoader()
